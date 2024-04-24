0a61c9769710d3736450096ade68ca3dcd8c2668
VladTca <votsats@gmail.com>
2024-04-24 03:03:34 +0200

    Added LeftSidebar styles and implemented Logo component with scroll functionality.

--- src/layout/leftsidebar/LeftSidebar.tsx
+++ src/layout/leftsidebar/LeftSidebar.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {S} from "./LeftSidebar_Styles";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
+import {Icon} from "../../components/icon/Icon";
 
 export const LeftSidebar = () => {
     return (
-        <div>
-       sdfgsdfg
-        </div>
+        <S.LeftSidebar>
+            <Container>
+                <FlexWrapper direction={'column'} >
+                    <Icon iconId={"logo"} />
+                    <Icon iconId={"logo"} />
+                    <Icon iconId={"logo"} />
+                    <Icon iconId={"logo"} />
+                </FlexWrapper>
+            </Container>
+        </S.LeftSidebar>
     );
 };
---
+++ src/layout/leftsidebar/LeftSidebar_Styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const LeftSidebar = styled.aside`
+
+    background-color: rgba(228, 228, 228, 1);
+    padding: 20px 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+
+`;
+
+
+export const S = {
+    LeftSidebar,
+
+}
