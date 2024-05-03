1c4bd9517fa7ddde6ea0325ad124524bf5de7825
VladTca <votsats@gmail.com>
2024-05-03 04:32:10 +0200

    Refactor App.tsx to include react-router-dom for routing, add TableChoice component, and configure routes. Add babel plugins for TypeScript support, styled-components, and private property in object proposal. Update package.json with necessary dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,27 @@ import { Header } from './layout/header/Header';
 import { LeftSidebar } from './layout/leftsidebar/LeftSidebar';
 import {RightSidebar} from "./layout/rightSidebar/RightSidebar";
 import {Main} from "./layout/main/Main";
+import {BrowserRouter, Routes, Route,Link} from "react-router-dom";
+import TableChoice from "./layout/tablechoice/TableChoice";
 
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <LeftSidebar/>
-        <RightSidebar/>
-        <Main/>
-    </div>
+
+      <BrowserRouter>
+
+          <Routes>
+                <Route path="/" element={<TableChoice/>}/>
+                <Route path="/tablechoice" element={<h1>Это вторая страница</h1>}/>
+
+
+              <div className="App">
+                  <Header/>
+                  <LeftSidebar/>
+                  <RightSidebar/>
+                  <Main/>
+              </div>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/layout/tablechoice/TableChoice.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TableChoice = () => {
+    return <h1>Это вторая страница</h1>;
+};
+
+export default TableChoice;
